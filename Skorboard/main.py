6d398120a449bd935beb55eae6cc2ed5ffba9d0d
ezra <ezra.alvansga@polytron.co.id>
2023-04-14 09:41:07 +0700

    usable skorboad table tennis

--- Skorboard/main.py
+++ Skorboard/main.py
@@ -2,7 +2,17 @@
 import pygame
 import evdev
 dev = evdev.InputDevice('/dev/input/event3')
- 
+import time
+
+# CUSTOMIZATION
+CHGSERVAFTER = 2 # 2 or 5
+WIN_WIDTH = 1366
+WIN_HEIGTH = 768
+L_COLOR = [150,0,0]#[180,0,0]
+R_COLOR = [0,0,150]#[0,0,180]
+SKORLIST_COLOR = [255,255,255]#[0,0,180]
+CIRCLE_COLOR = [255,255,255]
+# -------------
 
 IRKEY_EXIT = "0x804b"
 IRKEY_MENU = "0x800b"
@@ -12,55 +22,106 @@ IRKEY_DOWN = "0x800d"
 IRKEY_RIGHT = "0x800e"
 IRKEY_LEFT = "0x800f"
 
+#IRKEY_SELECT = "0x8014"
+IRKEY_PRDOWN = "0x8018"
+
+IRKEY_NUM0 = "0x8000"
+IRKEY_NUM1 = "0x8001"
+IRKEY_NUM3 = "0x8003"
+IRKEY_NUM5 = "0x8005"
+IRKEY_NUM7 = "0x8007"
+IRKEY_NUM9 = "0x8009"
+
+TEAM_LEFT , TEAM_RIGHT = 0 , 1
+
 class Game():
     def __init__(self):
         self.serv = 0
         self.skor = [[0,0]]
 
-def updateScreen(screen, game):
-    pygame.draw.rect(screen, (150,0,0), pygame.Rect(0, 0, 1366//2, 768))
-    pygame.draw.rect(screen, (0,0,150), pygame.Rect(1366//2, 0, 1366//2, 768))
-    pygame.draw.rect(screen, (100,0,0), pygame.Rect(0, 0, 1366//2, 64))
-    pygame.draw.rect(screen, (0,0,100), pygame.Rect(1366//2, 0, 1366//2, 64))
+    def reset(self):
+        self.serv = 0
+        self.skor = [[0,0]]
 
-    font = pygame.font.Font('freesansbold.ttf', 512)
-    text = font.render('0', True, (0,0,0))
+def checkService(game):
+    if sum(game.skor[len(game.skor)-1]) % CHGSERVAFTER == 0:
+        game.serv = ~game.serv #toggle
+
+def changeCourt(game):
+    global L_COLOR, R_COLOR
+    L_COLOR , R_COLOR = R_COLOR , L_COLOR
+    for ele in game.skor:
+        ele[0] , ele[1] = ele[1] , ele[0]
+
+def updateScreen(screen, game):
+    pygame.draw.rect(screen, L_COLOR, pygame.Rect(0, 0, WIN_WIDTH//2, WIN_HEIGTH))
+    pygame.draw.rect(screen, R_COLOR, pygame.Rect(WIN_WIDTH//2, 0, WIN_WIDTH//2, WIN_HEIGTH))
+
+    top = pygame.Surface((WIN_WIDTH, WIN_HEIGTH//24))
+    top.set_alpha(128)
+    top.fill((0,0,0))
+    screen.blit(top,(0,0))
+
+    font = pygame.font.Font('freesansbold.ttf', int(WIN_HEIGTH/1.5))
+    if sum(L_COLOR) < 180:
+        txt_color = (255,255,255)
+    else:
+        txt_color = (0,0,0)
+    text = font.render(str(game.skor[len(game.skor)-1][TEAM_LEFT]), True, txt_color)
     textRect = text.get_rect()
-    textRect.center = (1366//2 // 2, 64 + 768 // 2)
+    textRect.center = (WIN_WIDTH//2//2, WIN_HEIGTH//12 + WIN_HEIGTH//2)
     screen.blit(text, textRect)
 
-    text = font.render('0', True, (0,0,0))
+    if sum(R_COLOR) < 180:
+        txt_color = (255,255,255)
+    else:
+        txt_color = (0,0,0)
+    text = font.render(str(game.skor[len(game.skor)-1][TEAM_RIGHT]), True, txt_color)
     textRect = text.get_rect()
-    textRect.center = ((1366//2) + 1366//2 // 2, 64 + 768 // 2)
+    textRect.center = ((WIN_WIDTH//2) + WIN_WIDTH//2//2, WIN_HEIGTH//12 + WIN_HEIGTH//2)
+    screen.blit(text, textRect)
+
+    font = pygame.font.Font('freesansbold.ttf', int(WIN_HEIGTH//32))
+    skorlist = ""
+    try:
+        for i in range(len(game.skor) - 1):
+            skorlist += str(game.skor[i][0]) + "-" + str(game.skor[i][1]) + " "
+    except:
+        pass
+    text = font.render(skorlist, True, SKORLIST_COLOR)
+    textRect = text.get_rect()
+    textRect.left = 4
+    textRect.top = 4
     screen.blit(text, textRect)
 
     if game.serv == 0:
-        pygame.draw.circle(screen, (255,255,255), (1366//2 - 32, 64//2 ), 24)
-    elif game.serv == 1:
-        pygame.draw.circle(screen, (255,255,255), (1366//2 + 32, 64//2 ), 24)
+        pygame.draw.circle(screen, CIRCLE_COLOR, (WIN_WIDTH//2 - WIN_HEIGTH//24, WIN_HEIGTH//12//2 + WIN_HEIGTH//24), WIN_HEIGTH//32)
+    elif game.serv == -1:
+        pygame.draw.circle(screen, CIRCLE_COLOR, (WIN_WIDTH//2 + WIN_HEIGTH//24, WIN_HEIGTH//12//2 + WIN_HEIGTH//24), WIN_HEIGTH//32)
 
     pygame.display.flip()
 
 # define a main function
 def main():
-     
     # initialize the pygame module
     pygame.init()
     # load and set the logo
     logo = pygame.image.load("logo32x32.png")
     pygame.display.set_icon(logo)
-    pygame.display.set_caption("minimal program")
+    pygame.display.set_caption("Score Board")
      
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((1366,768) , pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGTH) , pygame.FULLSCREEN)
      
     # define a variable to control the main loop
     running = True
     
     room = Game()
+    stamp_time = time.time()
 
     # main loop
     while running:
+        #print(".")
         updateScreen(screen, room)
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
@@ -79,7 +140,7 @@ def main():
                 pass
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT):
                 # screen.fill((0,0,128))
-                room.serv = 1
+                room.serv = -1
                 updateScreen(screen, room)
                 pass
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT):
@@ -87,9 +148,60 @@ def main():
                 room.serv = 0
                 updateScreen(screen, room)
                 pass
+            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_1):
+                # screen.fill((128,0,0))
+                room.skor[len(room.skor)-1][TEAM_LEFT] += 1
+                checkService(room)
+                updateScreen(screen, room)
+                pass
+            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_3):
+                # screen.fill((128,0,0))
+                room.skor[len(room.skor)-1][TEAM_RIGHT] += 1
+                checkService(room)
+                updateScreen(screen, room)
+                pass
+            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_7):
+                # screen.fill((128,0,0))
+                room.skor[len(room.skor)-1][TEAM_LEFT] -= 1
+                checkService(room)
+                updateScreen(screen, room)
+                pass
+            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_9):
+                # screen.fill((128,0,0))
+                room.skor[len(room.skor)-1][TEAM_RIGHT] -= 1
+                checkService(room)
+                updateScreen(screen, room)
+                pass
+            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_5):
+                # screen.fill((128,0,0))
+                room.skor.append([0,0])
+                checkService(room)
+                updateScreen(screen, room)
+                print(room.skor)
+                pass
+            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_r):
+                # screen.fill((128,0,0))
+                room.reset()
+                checkService(room)
+                updateScreen(screen, room)
+                pass
+            elif (event.type == pygame.KEYDOWN and event.key == pygame.K_s):
+                # screen.fill((128,0,0))
+                changeCourt(room)
+                updateScreen(screen, room)
+                print(room.skor)
+                pass
+            # if event.type == pygame.KEYDOWN:
+            #     print(room.skor)
+            #     print(event.key)
 
         for event in dev.read_loop():
-        #	print(evdev.categorize(event))
+        #	print(evdev.categorize(event)) 
+            if time.time() - stamp_time < 0.5:
+                print("after 0.5s")
+                break
+            stamp_time = time.time()
+            
             if hex(event.value) == "0x0":
                 continue
             # elif hex(event.value) == IRKEY_EXIT:
@@ -107,19 +219,65 @@ def main():
                 pass
             elif hex(event.value) == IRKEY_RIGHT:
                 # screen.fill((0,0,128))
-                room.serv = 1
-                updateScreen(screen, room)
+                serv = 1
+                #updateScreen(screen, serv)
+                break
                 pass
             elif hex(event.value) == IRKEY_LEFT:
                 # screen.fill((128,0,128))
-                room.serv = 0
-                updateScreen(screen, room)
+                serv = 0
+                #updateScreen(screen, serv)
+                break
                 pass
-            
+            elif hex(event.value) == IRKEY_NUM1:
+                # screen.fill((0,128,0))
+                room.skor[len(room.skor)-1][TEAM_LEFT] += 1
+                checkService(room)
+                #updateScreen(screen, room)
+                break
+                pass
+            elif hex(event.value) == IRKEY_NUM3:
+                room.skor[len(room.skor)-1][TEAM_RIGHT] += 1
+                checkService(room)
+                #updateScreen(screen, room)
+                break
+                pass
+            elif hex(event.value) == IRKEY_NUM7:
+                room.skor[len(room.skor)-1][TEAM_LEFT] -= 1
+                checkService(room)
+                #updateScreen(screen, room)
+                break
+                pass
+            elif hex(event.value) == IRKEY_NUM9:
+                room.skor[len(room.skor)-1][TEAM_RIGHT] -= 1
+                checkService(room)
+                #updateScreen(screen, room)
+                break
+                pass
+            elif hex(event.value) == IRKEY_NUM5:
+                room.skor.append([0,0])
+                checkService(room)
+                #updateScreen(screen, room)
+                break
+                print(room.skor)
+                pass
+            elif hex(event.value) == IRKEY_PRDOWN:
+                room.reset()
+                checkService(room)
+                #updateScreen(screen, room)
+                break
+                pass
+            elif hex(event.value) == IRKEY_NUM0:
+                changeCourt(room)
+                #updateScreen(screen, room)
+                print(room.skor)
+                break
+                pass
+               
         #     print("\nCode:",hex(event.value))
         #     print("---------------")
-        #     pass
-     
+            pass
+            
      
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
