5e4942e63f2dac4a322b5dc9febb51f85370c83e
ezra <ezra.alvansga@polytron.co.id>
2023-04-14 10:30:27 +0700

    all function goto method

--- Skorboard/win_main.py
+++ Skorboard/win_main.py
@@ -32,63 +32,63 @@ class Game():
         self.serv = 0
         self.skor = [[0,0]]
 
-def checkService(game):
-    if sum(game.skor[len(game.skor)-1]) % CHGSERVAFTER == 0:
-        game.serv = ~game.serv #toggle
+    def checkService(game): #first arguments of method always consider as self
+        if sum(game.skor[len(game.skor)-1]) % CHGSERVAFTER == 0:
+            game.serv = ~game.serv #toggle
 
-def changeCourt(game):
-    global L_COLOR, R_COLOR
-    L_COLOR , R_COLOR = R_COLOR , L_COLOR
-    for ele in game.skor:
-        ele[0] , ele[1] = ele[1] , ele[0]
+    def changeCourt(game):
+        global L_COLOR, R_COLOR
+        L_COLOR , R_COLOR = R_COLOR , L_COLOR
+        for ele in game.skor:
+            ele[0] , ele[1] = ele[1] , ele[0]
 
-def updateScreen(screen, game):
-    pygame.draw.rect(screen, L_COLOR, pygame.Rect(0, 0, WIN_WIDTH//2, WIN_HEIGTH))
-    pygame.draw.rect(screen, R_COLOR, pygame.Rect(WIN_WIDTH//2, 0, WIN_WIDTH//2, WIN_HEIGTH))
+    def updateScreen(game, screen):
+        pygame.draw.rect(screen, L_COLOR, pygame.Rect(0, 0, WIN_WIDTH//2, WIN_HEIGTH))
+        pygame.draw.rect(screen, R_COLOR, pygame.Rect(WIN_WIDTH//2, 0, WIN_WIDTH//2, WIN_HEIGTH))
 
-    top = pygame.Surface((WIN_WIDTH, WIN_HEIGTH//24))
-    top.set_alpha(128)
-    top.fill((0,0,0))
-    screen.blit(top,(0,0))
+        top = pygame.Surface((WIN_WIDTH, WIN_HEIGTH//24))
+        top.set_alpha(128)
+        top.fill((0,0,0))
+        screen.blit(top,(0,0))
 
-    font = pygame.font.Font('freesansbold.ttf', int(WIN_HEIGTH/1.5))
-    if sum(L_COLOR) < 180:
-        txt_color = (255,255,255)
-    else:
-        txt_color = (0,0,0)
-    text = font.render(str(game.skor[len(game.skor)-1][TEAM_LEFT]), True, txt_color)
-    textRect = text.get_rect()
-    textRect.center = (WIN_WIDTH//2//2, WIN_HEIGTH//12 + WIN_HEIGTH//2)
-    screen.blit(text, textRect)
+        font = pygame.font.Font('freesansbold.ttf', int(WIN_HEIGTH/1.5))
+        if sum(L_COLOR) < 180:
+            txt_color = (255,255,255)
+        else:
+            txt_color = (0,0,0)
+        text = font.render(str(game.skor[len(game.skor)-1][TEAM_LEFT]), True, txt_color)
+        textRect = text.get_rect()
+        textRect.center = (WIN_WIDTH//2//2, WIN_HEIGTH//12 + WIN_HEIGTH//2)
+        screen.blit(text, textRect)
 
-    if sum(R_COLOR) < 180:
-        txt_color = (255,255,255)
-    else:
-        txt_color = (0,0,0)
-    text = font.render(str(game.skor[len(game.skor)-1][TEAM_RIGHT]), True, txt_color)
-    textRect = text.get_rect()
-    textRect.center = ((WIN_WIDTH//2) + WIN_WIDTH//2//2, WIN_HEIGTH//12 + WIN_HEIGTH//2)
-    screen.blit(text, textRect)
+        if sum(R_COLOR) < 180:
+            txt_color = (255,255,255)
+        else:
+            txt_color = (0,0,0)
+        text = font.render(str(game.skor[len(game.skor)-1][TEAM_RIGHT]), True, txt_color)
+        textRect = text.get_rect()
+        textRect.center = ((WIN_WIDTH//2) + WIN_WIDTH//2//2, WIN_HEIGTH//12 + WIN_HEIGTH//2)
+        screen.blit(text, textRect)
 
-    font = pygame.font.Font('freesansbold.ttf', int(WIN_HEIGTH//32))
-    skorlist = ""
-    try:
-        for i in range(len(game.skor) - 1):
-            skorlist += str(game.skor[i][0]) + "-" + str(game.skor[i][1]) + " "
-    except:
-        pass
-    text = font.render(skorlist, True, SKORLIST_COLOR)
-    textRect = text.get_rect()
-    textRect.left = 4
-    textRect.top = 4
-    screen.blit(text, textRect)
+        font = pygame.font.Font('freesansbold.ttf', int(WIN_HEIGTH//32))
+        skorlist = ""
+        try:
+            for i in range(len(game.skor) - 1):
+                skorlist += str(game.skor[i][0]) + "-" + str(game.skor[i][1]) + " "
+        except:
+            pass
+        text = font.render(skorlist, True, SKORLIST_COLOR)
+        textRect = text.get_rect()
+        textRect.left = 4
+        textRect.top = 4
+        screen.blit(text, textRect)
 
-    if game.serv == 0:
-        pygame.draw.circle(screen, CIRCLE_COLOR, (WIN_WIDTH//2 - WIN_HEIGTH//24, WIN_HEIGTH//12//2 + WIN_HEIGTH//24), WIN_HEIGTH//32)
-    elif game.serv == -1:
-        pygame.draw.circle(screen, CIRCLE_COLOR, (WIN_WIDTH//2 + WIN_HEIGTH//24, WIN_HEIGTH//12//2 + WIN_HEIGTH//24), WIN_HEIGTH//32)
+        if game.serv == 0:
+            pygame.draw.circle(screen, CIRCLE_COLOR, (WIN_WIDTH//2 - WIN_HEIGTH//24, WIN_HEIGTH//12//2 + WIN_HEIGTH//24), WIN_HEIGTH//32)
+        elif game.serv == -1:
+            pygame.draw.circle(screen, CIRCLE_COLOR, (WIN_WIDTH//2 + WIN_HEIGTH//24, WIN_HEIGTH//12//2 + WIN_HEIGTH//24), WIN_HEIGTH//32)
 
-    pygame.display.flip()
+        pygame.display.flip()
 
 # define a main function
 def main():
@@ -109,7 +109,7 @@ def main():
 
     # main loop
     while running:
-        updateScreen(screen, room)
+        room.updateScreen(screen)
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
@@ -135,31 +135,31 @@ def main():
                 pass
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_w):
                 room.skor[len(room.skor)-1][TEAM_LEFT] += 1
-                checkService(room)
+                room.checkService()
                 pass
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_i):
                 room.skor[len(room.skor)-1][TEAM_RIGHT] += 1
-                checkService(room)
+                room.checkService()
                 pass
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_s):
                 room.skor[len(room.skor)-1][TEAM_LEFT] -= 1
-                checkService(room)
+                room.checkService()
                 pass
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_k):
                 room.skor[len(room.skor)-1][TEAM_RIGHT] -= 1
-                checkService(room)
+                room.checkService()
                 pass
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN):
                 room.skor.append([0,0])
-                checkService(room)
+                room.checkService()
                 print(room.skor)
                 pass
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_n):
                 room.reset()
-                checkService(room)
+                room.checkService()
                 pass
             elif (event.type == pygame.KEYDOWN and event.key == pygame.K_t):
-                changeCourt(room)
+                room.changeCourt()
                 print(room.skor)
                 pass
             # if event.type == pygame.KEYDOWN:
